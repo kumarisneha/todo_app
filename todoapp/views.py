5ca95b7d10eee6d1bcc1aa970b70f1ecb70a10aa
Sneha Kumari <kumarisneha102@gmail.com>
2017-04-27 18:35:53 +0530

    Added drop down list for order in todo list

--- todoapp/views.py
+++ todoapp/views.py
@@ -14,9 +14,13 @@ def test(request):
         if task_date:
             task_date=datetime.datetime.strptime(task_date, '%Y-%m-%d %H:%M')
         a = Todolist(task = task_data, priority = priority, due_date= task_date)
-        a.save()       
-    sample = {'object_list': Todolist.objects.all().order_by('priority')}
-    return render(request, 'index.html', sample)
+        a.save()  
+    choose = request.GET.get('choose',1)
+    if int(choose) == 2 :
+        context={'todo_list': Todolist.objects.all().order_by('due_date')}
+    else:
+        context={'todo_list': Todolist.objects.all().order_by('priority')}     
+    return render(request, 'index.html', context)
 
 def delete_item(request, id):
     obj=Todolist.objects.get(id=id)
