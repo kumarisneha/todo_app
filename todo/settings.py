7a6bc73f7efa217f4a481c7a6d8cfbca20da7252
Sneha Kumari <kumarisneha102@gmail.com>
2017-05-09 17:33:11 +0530

    sending email with django

--- todo/settings.py
+++ todo/settings.py
@@ -121,13 +121,13 @@ CELERY_TIMEZONE = 'UTC'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
