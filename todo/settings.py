a91cd43ae3ef294fa8c342460cfd8dd845a8ffd6
Sneha Kumari <kumarisneha102@gmail.com>
2017-05-18 10:01:44 +0530

    changed static path

--- todo/settings.py
+++ todo/settings.py
@@ -133,5 +133,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
