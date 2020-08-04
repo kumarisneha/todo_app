0a4bcdf296bff068b27d2338c9e14d8ada571471
kumarisneha <kumarisneha102@gmail.com>
2020-08-04 18:08:17 +0530

    Now objects will have username

--- todoapp/models.py
+++ todoapp/models.py
@@ -11,14 +11,18 @@ class Registration(models.Model):
     password= models.CharField(max_length = 50)
     email_verified = models.BooleanField(default=False)
     email_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user_name
     
 class Todolist(models.Model):
-	task = models.CharField(max_length=200, null=True)
-	priority = models.IntegerField(default= '3')
-	due_date = models.DateTimeField(null=True)
-	person = models.ForeignKey(Registration, blank=True, null=True, on_delete=models.SET_NULL)
+    task = models.CharField(max_length=200, null=True)
+    priority = models.IntegerField(default= '3')
+    due_date = models.DateTimeField(null=True)
+    person = models.ForeignKey(Registration, blank=True, null=True, on_delete=models.SET_NULL)
 
-
-	
-	
+    def __str__(self):
+        return self.task
+    
+    
 
